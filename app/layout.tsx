7951a37808b3a15ccc34417993cf7d3e726c4e1d
MulyaKoding <rahmatmulya91@gmail.com>
2026-04-02 10:57:50 +0700

    Adj: pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,64 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* ── PWA ── */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#228CD1" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="format-detection" content="telephone=no" />
+
+        {/* ── Icons ── */}
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="48x48"
+          href="/icons/icons-48x48.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="72x72"
+          href="/icons/icons-72x72.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/icons/icons-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="128x128"
+          href="/icons/icons-128x128.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="144x144"
+          href="/icons/icons-144x144.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/icons/icons-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="384x384"
+          href="/icons/icons-384x384.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/icons/icons-512x512.png"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
